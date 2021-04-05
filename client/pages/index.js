a3766edc792d63846ef9dc3b9e13f0c3cbf9a543
exoticvelvet <1198275509@qq.com>
2021-04-05 10:45:32 +0100

    create landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,14 @@
-export default () => {
-  return <h1>Landing page</h1>
-}
+// import axios from "axios";
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => {
+  console.info(currentUser);
+  return <h1>Landing page</h1>;
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+  const { data } = await buildClient({ req }).get("/api/users/currentuser");
+  return data;
+};
+
+export default LandingPage;
